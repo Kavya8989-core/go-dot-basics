ee3c025ec6e96713ddbeb3ebb85841605d1f0202
KAVYA SHARMA <ks2371008@gmail.com>
2026-08-05 13:08:18 +0530

    jump , run and idle animation implemented

--- sceans/main_character.gd
+++ sceans/main_character.gd
@@ -3,9 +3,16 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+@onready var sprite_2d: AnimatedSprite2D = $Sprite2D
 
 
 func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		sprite_2d.animation="jump"
+	elif(velocity.x> 1 or velocity.x< -1):
+		sprite_2d.animation="running"
+	else:
+		sprite_2d.animation="default"
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -13,6 +20,7 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		sprite_2d.animation="jump"
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -23,3 +31,6 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	
+	var left=velocity.x<0
+	sprite_2d.flip_h=left
